d927742ff16730823ea00413ffbd2c02485fc1a0
Jazfer29 <143872199+Jazfer29@users.noreply.github.com>
2024-03-13 14:49:02 +0800

    Initial Update

--- TypingGameInter/Form1.Designer.cs
+++ TypingGameInter/Form1.Designer.cs
@@ -62,9 +62,10 @@
             this.lblright.AutoSize = true;
             this.lblright.BackColor = System.Drawing.Color.Transparent;
             this.lblright.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblright.Location = new System.Drawing.Point(327, 52);
+            this.lblright.Location = new System.Drawing.Point(409, 65);
+            this.lblright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblright.Name = "lblright";
-            this.lblright.Size = new System.Drawing.Size(76, 20);
+            this.lblright.Size = new System.Drawing.Size(94, 25);
             this.lblright.TabIndex = 0;
             this.lblright.Text = "Score: 0";
             // 
@@ -73,9 +74,10 @@
             this.lblwrong.AutoSize = true;
             this.lblwrong.BackColor = System.Drawing.Color.Transparent;
             this.lblwrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblwrong.Location = new System.Drawing.Point(561, 52);
+            this.lblwrong.Location = new System.Drawing.Point(701, 65);
+            this.lblwrong.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblwrong.Name = "lblwrong";
-            this.lblwrong.Size = new System.Drawing.Size(91, 20);
+            this.lblwrong.Size = new System.Drawing.Size(112, 25);
             this.lblwrong.TabIndex = 1;
             this.lblwrong.Text = "Mistake: 0";
             // 
@@ -83,9 +85,10 @@
             // 
             this.lblword.BackColor = System.Drawing.Color.Transparent;
             this.lblword.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblword.Location = new System.Drawing.Point(324, 120);
+            this.lblword.Location = new System.Drawing.Point(405, 150);
+            this.lblword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblword.Name = "lblword";
-            this.lblword.Size = new System.Drawing.Size(328, 84);
+            this.lblword.Size = new System.Drawing.Size(410, 105);
             this.lblword.TabIndex = 2;
             this.lblword.Text = "Word";
             this.lblword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -95,18 +98,20 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(398, 272);
+            this.label4.Location = new System.Drawing.Point(498, 340);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(181, 20);
+            this.label4.Size = new System.Drawing.Size(221, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Enter to check the Word";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(324, 222);
+            this.textBox1.Location = new System.Drawing.Point(405, 278);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(328, 47);
+            this.textBox1.Size = new System.Drawing.Size(409, 57);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.CheckGame);
@@ -120,9 +125,10 @@
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(434, 310);
+            this.button1.Location = new System.Drawing.Point(542, 388);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.Size = new System.Drawing.Size(125, 44);
             this.button1.TabIndex = 5;
             this.button1.Text = "30 sec";
             this.button1.UseVisualStyleBackColor = true;
@@ -133,9 +139,10 @@
             this.lblScreen.BackColor = System.Drawing.Color.Transparent;
             this.lblScreen.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblScreen.ForeColor = System.Drawing.Color.Red;
-            this.lblScreen.Location = new System.Drawing.Point(422, 60);
+            this.lblScreen.Location = new System.Drawing.Point(528, 75);
+            this.lblScreen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblScreen.Name = "lblScreen";
-            this.lblScreen.Size = new System.Drawing.Size(135, 60);
+            this.lblScreen.Size = new System.Drawing.Size(169, 75);
             this.lblScreen.TabIndex = 6;
             this.lblScreen.Text = "0";
             this.lblScreen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -144,18 +151,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(293, 2);
+            this.label2.Location = new System.Drawing.Point(366, 2);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(304, 36);
+            this.label2.Size = new System.Drawing.Size(382, 45);
             this.label2.TabIndex = 13;
             this.label2.Text = "RAPID TYPER TESTER";
             // 
             // button2
             // 
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(732, 12);
+            this.button2.Location = new System.Drawing.Point(915, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 14;
             this.button2.Text = "Back";
             this.button2.UseVisualStyleBackColor = true;
@@ -166,9 +175,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.Size = new System.Drawing.Size(75, 25);
             this.label1.TabIndex = 15;
             this.label1.Text = "Name:";
             // 
@@ -177,9 +187,11 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(203, 396);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(254, 495);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(624, 36);
+            this.label5.Size = new System.Drawing.Size(785, 45);
             this.label5.TabIndex = 18;
             this.label5.Text = "Click \"Timer\" first before you enter the word";
             // 
@@ -187,9 +199,10 @@
             // 
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(332, 310);
+            this.button3.Location = new System.Drawing.Point(415, 388);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 35);
+            this.button3.Size = new System.Drawing.Size(125, 44);
             this.button3.TabIndex = 19;
             this.button3.Text = "15 Sec";
             this.button3.UseVisualStyleBackColor = true;
@@ -199,9 +212,10 @@
             // 
             this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(539, 310);
+            this.button4.Location = new System.Drawing.Point(674, 388);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 35);
+            this.button4.Size = new System.Drawing.Size(125, 44);
             this.button4.TabIndex = 20;
             this.button4.Text = "60 sec";
             this.button4.UseVisualStyleBackColor = true;
@@ -212,9 +226,10 @@
             this.lbltext.AutoSize = true;
             this.lbltext.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lbltext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbltext.Location = new System.Drawing.Point(74, 15);
+            this.lbltext.Location = new System.Drawing.Point(92, 19);
+            this.lbltext.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbltext.Name = "lbltext";
-            this.lbltext.Size = new System.Drawing.Size(0, 20);
+            this.lbltext.Size = new System.Drawing.Size(0, 25);
             this.lbltext.TabIndex = 21;
             // 
             // NameLabel
@@ -222,9 +237,10 @@
             this.NameLabel.AutoSize = true;
             this.NameLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NameLabel.Location = new System.Drawing.Point(74, 12);
+            this.NameLabel.Location = new System.Drawing.Point(92, 15);
+            this.NameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NameLabel.Name = "NameLabel";
-            this.NameLabel.Size = new System.Drawing.Size(55, 20);
+            this.NameLabel.Size = new System.Drawing.Size(68, 25);
             this.NameLabel.TabIndex = 22;
             this.NameLabel.Text = "Name";
             // 
@@ -239,13 +255,13 @@
             this.dgvScores,
             this.dgvMistakes,
             this.dgvChosenTime});
-            this.dataGridView1.Location = new System.Drawing.Point(5, 55);
+            this.dataGridView1.Location = new System.Drawing.Point(6, 69);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(307, 333);
+            this.dataGridView1.Size = new System.Drawing.Size(384, 416);
             this.dataGridView1.TabIndex = 23;
             // 
             // dgvID
@@ -299,17 +315,17 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(963, 46);
+            this.panel1.Size = new System.Drawing.Size(1204, 58);
             this.panel1.TabIndex = 24;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(963, 464);
+            this.ClientSize = new System.Drawing.Size(1204, 580);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lbltext);
             this.Controls.Add(this.button4);
@@ -324,6 +340,7 @@
             this.Controls.Add(this.lblright);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Speed Typing Easy";
